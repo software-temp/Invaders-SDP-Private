ebaf5bcb6d4c59c14be80a43747b50471c7fddcb
hums <hum4310@naver.com>
2025-11-06 13:10:40 +0900

    feat: init achievement Controller and View

---
+++ src/engine/AchievementController.java
@@ -0,0 +1,18 @@
+package engine;
+
+import screen.Screen;
+
+import java.awt.event.KeyEvent;
+
+public class AchievementController implements IController{
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
---
+++ src/screen/AchievementView.java
@@ -0,0 +1,26 @@
+package screen;
+import engine.IController;
+import screen.Screen;
+
+import java.awt.event.KeyEvent;
+
+public class AchievementView extends Screen implements IView {
+    public AchievementView(int width, int height, int fps){
+        super(width, height, fps);
+    }
+    @Override
+    public void loadAssets() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
+
